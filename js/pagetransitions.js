9a7a4d18ae98efbda1928f855f2a12435a7a9dca
Bart Middag <bart.middag@gmail.com>
2016-10-12 16:14:22 +0200

    Initial commit: Layout only -- no content just yet!

---
+++ js/pagetransitions.js
@@ -0,0 +1,103 @@
+var PageTransitions = (function() {
+
+	var $main = $( '#pt-main' ),
+		$pages = $main.children( 'div.pt-page' ),
+		$changePage = $( '.menu-item' ),
+		current = 0,
+		isAnimating = false,
+		endCurrPage = false,
+		endNextPage = false,
+		animEndEventNames = {
+			'WebkitAnimation' : 'webkitAnimationEnd',
+			'OAnimation' : 'oAnimationEnd',
+			'msAnimation' : 'MSAnimationEnd',
+			'animation' : 'animationend'
+		},
+		// animation end event name
+		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
+		// support css animations
+		support = Modernizr.cssanimations;
+	
+	function init() {
+
+		$pages.each( function() {
+			var $page = $( this );
+			$page.data( 'originalClassList', $page.attr( 'class' ) );
+		} );
+
+		$pages.eq( current ).addClass( 'pt-page-current' );
+
+		$( '#dl-menu' ).dlmenu( {
+			animationClasses : { in : 'dl-animate-in-2', out : 'dl-animate-out-2' },
+			onLinkClick : function( el, ev ) {
+				ev.preventDefault();
+				nextPage( el.data( 'animation' ) );
+			}
+		} );
+
+		$changePage.on( 'click', function() {
+			if( isAnimating ) return false;
+			
+			outClass = 'pt-page-moveToRight';
+			inClass = 'pt-page-moveFromLeft';
+			// outClass = 'pt-page-fade';
+			// inClass = 'pt-page-moveFromLeft pt-page-ontop';
+			
+			nextPage( $( this ).attr("page"), outClass, inClass);
+		} );
+
+	}
+
+	function nextPage(targetPage, outClass, inClass) {
+		if(isAnimating) return false;
+		var $currPage = $pages.eq(current);
+		var $nextPage = $pages.filter(targetPage);
+		if($currPage.is($nextPage)) return false;
+		
+		isAnimating = true;
+		$nextPage.addClass( 'pt-page-current' );
+		current = $nextPage.index();
+		console.log($pages.children(targetPage));
+
+		$currPage.addClass( outClass ).on( animEndEventName, function() {
+			$currPage.off( animEndEventName );
+			endCurrPage = true;
+			if( endNextPage ) {
+				onEndAnimation( $currPage, $nextPage );
+			}
+		} );
+
+		$nextPage.addClass( inClass ).on( animEndEventName, function() {
+			$nextPage.off( animEndEventName );
+			endNextPage = true;
+			if( endCurrPage ) {
+				onEndAnimation( $currPage, $nextPage );
+			}
+		} );
+
+		if( !support ) {
+			onEndAnimation( $currPage, $nextPage );
+		}
+
+	}
+
+	function onEndAnimation( $outpage, $inpage ) {
+		endCurrPage = false;
+		endNextPage = false;
+		resetPage( $outpage, $inpage );
+		isAnimating = false;
+	}
+
+	function resetPage( $outpage, $inpage ) {
+		$outpage.attr( 'class', $outpage.data( 'originalClassList' ) );
+		$inpage.attr( 'class', $inpage.data( 'originalClassList' ) + ' pt-page-current' );
+	}
+
+	init();
+
+	return { 
+		init : init,
+		nextPage : nextPage,
+	};
+
+})();
